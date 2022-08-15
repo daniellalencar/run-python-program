acd5e1f8606b9b11354574c964030861d60e8cd8
daniellalencar <daniel.lima.alencar@gmail.com>
2022-08-15 17:16:21 -0300

    First Commit

--- src/main/java/br/com/pcdf/ThreadController.java
+++ src/main/java/br/com/pcdf/ThreadController.java
@@ -3,7 +3,6 @@ package br.com.pcdf;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class ThreadController {
@@ -12,48 +11,18 @@ public class ThreadController {
   private static int lastValue = 10;
 
 
-  private void tet() {
-    List testlist = new ArrayList();
-    testlist.add("test1");
-    testlist.add("test2");
+  public static void main(String[] args) {
+    ThreadController threadController = new ThreadController();
+    threadController.execute();
   }
 
   private void execute() {
-    int[] numbers = new int[100000];
-    Random rnd = new Random();
-    for (int index = 0; index < numbers.length; index++) {
-      numbers[index] = rnd.nextInt();
-    }
-    Thread[] threads = new Thread[10];
-    Worker[] workers = new Worker[10];
-    int range = numbers.length / 10;
-    for (int index = 0; index < 10; index++) {
-      int startAt = index * range;
-      int endAt = startAt + range;
-      //workers[index] = new Worker(startAt, endAt, numbers);
-    }
-    for (int index = 0; index < 10; index++) {
-      threads[index] = new Thread(workers[index]);
-      threads[index].start();
-    }
-    boolean isProcessing = false;
-    do {
-      isProcessing = false;
-      for (Thread t : threads) {
-        if (t.isAlive()) {
-          isProcessing = true;
-          break;
-        }
-      }
-    } while (isProcessing);
-    for (Worker worker : workers) {
-     // System.out.println("Max = " + worker.getMax());
-    }
-  }
-
-  private void execute2() {
     CommandsList commandsList = new CommandsList();
     final List<String> commandList = commandsList.getCommandList();
+    executeThreads(commandList);
+  }
+
+  private void executeThreads(final List<String> commandList) {
 
     List<String> list = commandList.subList(firstValue, lastValue);
     final List<Thread> threadList = list.stream()
@@ -72,7 +41,13 @@ public class ThreadController {
       }
     } while (isProcessing);
 
-    list = commandList.subList(firstValue + lastValue, lastValue + lastValue);
+    int newLastValue = firstValue + lastValue;
+    int newFinal = lastValue + lastValue;
+
+    if (newFinal <= commandList.size() && newLastValue < commandList.size()) {
+      final List<String> strings = commandList.subList(newLastValue, newFinal);
+      executeThreads(strings);
+    }
   }
 
   public static class Worker implements Runnable {
